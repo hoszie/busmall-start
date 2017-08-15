0cd36ad9c607374b7a5c1a9e07f530795b440bf9
nhoszko@gmail.com <nhoszko@gmail.com>
2017-08-15 08:38:21 -0700

    images rendered, previous repeat still stuffed

--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
 'use strict'
 
-function Item (displayName, filePath, displayCount, voteCount, id) {
+function Item (displayName, filePath, id) {
     this.displayName = displayName;
     this.filePath = filePath;
-    this.displayCount = displayCount;
-    this.voteCount = voteCount;
     this.id = id;
+    this.displayCount = 0;
+    this.voteCount = 0;
 }
 
+var bag = new Item ( 'Bag', 'images/bag.jpg', 'bag' );
+var banana = new Item ( 'Banana', 'images/banana.jpg', 'banana' );
+var bathroom = new Item ( 'Bathroom', 'images/bathroom.jpg', 'bathroom' );
+var boots = new Item ( 'Boots', 'images/boots.jpg', 'boots' );
+var breakfast = new Item ( 'Breakfast', 'images/breakfast.jpg', 'breakfast' );
+var bubbleGum = new Item ( 'Bubblegum', 'images/bubblegum.jpg', 'bubblegum' );
+var chair = new Item ( 'Chair', 'images/chair.jpg', 'chair' );
+var cthulhu = new Item ( 'Cthulhu', 'images/cthulhu.jpg', 'cthulhu' );
+var dogDuck = new Item ( 'Dog-Duck', 'images/dog-duck.jpg', 'dog-duck' );
+var dragon = new Item ( 'Dragon', 'images/dragon.jpg', 'dragon' );
+var pen = new Item ( 'Pen', 'images/pen.jpg', 'pen' );
+var petSweep = new Item ( 'Pet Sweep', 'images/pet-sweep.jpg', 'pet-sweep' );
+var scissors = new Item ( 'Scissors', 'images/scissors.jpg', 'scissors' );
+var shark = new Item ( 'Shark', 'images/shark.jpg', 'shark' );
+var sweep = new Item ( 'Demeaning Baby Sweeper', 'images/sweep.png', 'sweep' );
+var tauntaun = new Item ( 'Sick Sleeping Bag', 'images/tauntaun.jpg', 'tauntaun' );
+var unicorn = new Item ( 'Faker Meat than SPAM', 'images/unicorn.jpg', 'unicorn' );
+var usb = new Item ( 'USB', 'images/usb.gif', 'usb' );
+var waterCan = new Item ( 'Watering Can', 'images/water-can.jpg', 'water-can' );
+var wineGlass = new Item ( 'Wine Glass', 'images/wine-glass.jpg', 'wine-glass' );
 
-var imageArray = [ 'bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'chair.jpg' ];
+var imageArray = [ bag, banana, bathroom, boots, breakfast, bubbleGum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass ];
+
+
 var previousImages = [];
-
 
 function displayImages() {
     var threeRandomNumbers = [];
@@ -22,12 +43,36 @@ function displayImages() {
             threeRandomNumbers.push ( num );
         }  
     } while ( threeRandomNumbers.length < 3 );
-    return threeRandomNumbers;
+    return threeRandomNumbers;      
 };
-// displayImages();
+
+
+
+function renderImages() {
+    var threeNumbers = displayImages();  
+    var elOne = document.getElementById( 'one' );
+    elOne.setAttribute( 'src', imageArray[ threeNumbers[0] ].filePath );
+
+    var elOne = document.getElementById( 'two' );
+    elOne.setAttribute( 'src', imageArray[ threeNumbers[1] ].filePath );
+
+    var elOne = document.getElementById( 'three' );
+    elOne.setAttribute( 'src', imageArray[ threeNumbers[2] ].filePath );
+    
+    previousImages = displayImages();                    //getnonPreviousNunmbers
+};
 
 function getNonPreviousNumbers() {
     previousImages = displayImages();
 };
 getNonPreviousNumbers();
 
+
+var optionOne = document.getElementById ( 'one' );
+optionOne.addEventListener ( 'click', renderImages );
+
+var optionTwo = document.getElementById ( 'two' );
+optionTwo.addEventListener ( 'click', renderImages );
+
+var optionThree = document.getElementById ( 'three' );
+optionThree.addEventListener ( 'click', renderImages );
---
+++ user-stories.md
@@ -0,0 +1,31 @@
+# Focus Group Participant
+
+As a focus group participant, I want to click on an image to vote for it. 
+
+As a focus group participant, I want the page to repopulate with images. 
+
+As a focus group participant, I want to know when I have used up all of my votes. 
+
+As a focus group participant, I want to see the results of my votes after I have voted. 
+
+# Marketer
+
+As a marketer, I want to know what products the focus group has clicked the most.
+
+As a marketer, I want to know the percentage that each item was clicked.
+
+As a marketer, I want to see which items were clicked the most across multiple users.
+
+As a marketer, I want to see the percentages of each item so that we can prioritize what we put in our magazine.
+
+# Developer
+
+As a developer, I want to display three random images so that the user can click on them.
+
+As a developer, I want the three images shown to not appear in the next instance of the event.
+
+As a developer, I want to collect data from the users and compile it on a graph so that the marketers can clearly see it.
+
+As a developer, I want the user’s selections to stop after a certain amount of time. 
+
+As a developer, I want to write code that is legible and understandable for my future self and other programmers. 
